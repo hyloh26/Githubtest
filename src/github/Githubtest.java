fc7e929888bb750d040e5738cbff031eaffc09a6
user <user@DESKTOP-3C8JB53>
2026-08-26 17:45:01 +0900

    test

--- src/github/Githubtest.java
+++ src/github/Githubtest.java
@@ -5,6 +5,6 @@ public class Githubtest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello Git! test5");	}
+		System.out.println("Hello Git! test6");	}
 
 }
