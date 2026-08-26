4391b06b01772ee80f40e2246ed3fe8a14ba3f41
user <user@DESKTOP-3C8JB53>
2026-08-26 17:50:51 +0900

    0826테스트

--- src/github/Githubtest.java
+++ src/github/Githubtest.java
@@ -1,10 +1,16 @@
 package github;
 
 public class Githubtest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Hello Git! test6");	}
-
+    public static void main(String[] args) {
+        // DLP Test Credentials & PII
+        String awsAccessKey = "AKIAIOSFODNN7EXAMPLE";
+        String awsSecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+        String githubToken  = "ghp_1234567890abcdefghijklmnopqrstuvwxyz";
+        String rsaPrivateKey = "-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAz8313...TEST...KEY...DATA...\n-----END RSA PRIVATE KEY-----";
+        
+        String testRRN   = "900101-1234567";
+        String testPhone = "010-1234-5678";
+        
+        System.out.println("DLP Test Script Execution");
+    }
 }
