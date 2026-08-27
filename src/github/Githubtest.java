b4c8f219a8302627c4280e7e2652cba8df7d7407
user <user@DESKTOP-3C8JB53>
2026-08-27 13:26:25 +0900

    ttttttttttttttt

--- src/github/Githubtest.java
+++ src/github/Githubtest.java
@@ -4,10 +4,10 @@ public class Githubtest {
     public static void main(String[] args) {
         // DLP Test Credentials & PII
         String awsAccessKey = "AKIAIOSFOsstestEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-        String githubToken  = "ghp_1234567890abcyyynopqrstuvwxyz";
+        String githubToken  = "ghp_1234567test890abcyyynopqrstuvwxyz";
         String rsaPrivateKey = "-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAz8313...TEST...KEY...DATA...\n-----END RSA PRIVATE KEY-----";
         
-        String testRRN   = "900101-1234567";
+        String testRRN dddd = "900101-1234567";
         String testPhone = "010-1111-5678";
         
         System.out.println("DLP Test Script Execution");
