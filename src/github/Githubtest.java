311a517ccf1716771b677a0c63ad1c00e9397fc6
user <user@DESKTOP-3C8JB53>
2026-08-27 17:35:12 +0900

    TEST

--- src/github/Githubtest.java
+++ src/github/Githubtest.java
@@ -5,7 +5,7 @@ public class Githubtest {
         // DLP Test Credentials & PII
         String awsAccessKey = "AKIAIOSFOsstestEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
         String githubToken  = "guvwxyz";
-        String rsaPrivateKey = "-N RSA PAAKCAQEAz8313...TEST...KEY...DATA...\n-----END RSA PRIVATE KEY-----";
+        String rsaPrivateKey = "-N RSA PAATEST...KEY...DATA...\n-----END RSA PRIVATE KEY-----";
 
         String testRRN  = "900101-1234567";
         String testPhone = "010-1111-5678";
