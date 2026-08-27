26201844efbab35968e766205e6dd75a77b27f7f
user <user@DESKTOP-3C8JB53>
2026-08-27 13:28:34 +0900

    test

--- src/github/Githubtest.java
+++ src/github/Githubtest.java
@@ -6,7 +6,7 @@ public class Githubtest {
         String awsAccessKey = "AKIAIOSFOsstestEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
         String githubToken  = "ghp_1234567test890abcyyynopqrstuvwxyz";
         String rsaPrivateKey = "-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAz8313...TEST...KEY...DATA...\n-----END RSA PRIVATE KEY-----";
-        
+
         String testRRN dddd = "900101-1234567";
         String testPhone = "010-1111-5678";
         
